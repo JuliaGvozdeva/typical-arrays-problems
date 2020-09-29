549747c3d0353e3d689cea7a1f3b5a8635f4cfd6
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2020-09-29 23:25:41 +0400

    feat: do max, min ar average functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0){
+    return 0;
+  }
+  else {
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+  
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0){
+    return 0;
+  } else {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0){
+    return 0;
+  } else {
+    let average = 0;
+    for (let i = 0; i < array.length; i++) {
+      average += array[i];
+    }
+    
+    return average/array.length;
+  }
 }
